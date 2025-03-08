c8b992bd6085586f2a01ac4a3c542081ad3e3b89
محمد <mrg2233@g.coppellisd.com>
2025-03-07 19:23:44 -0600

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,12 +1,6 @@
 import random
 
-## 1 = Rock
-## 2 = Paper
-## 3 = Scissors
-cc = random.randint(1,3)
-pc = int(input("Rock(1), Paper(2), or Scissors(3)? "))
-    
-def GetWinner():
+def GetWinner(cc, pc):
     if cc - pc == -1:
         print("You win!")
     elif cc - pc == 1:
@@ -18,6 +12,12 @@ def GetWinner():
     elif cc - pc == 0:
         print("It's a tie!")
 
-GetWinner()
-print("Computer's choice:", cc)
-print("Your choice:", pc)
+## 1 = Rock
+## 2 = Paper
+## 3 = Scissors
+cc = random.randint(1,3)
+pc = int(input("Rock(1), Paper(2), or Scissors(3)? "))
+    
+GetWinner(cc, pc)
+print(f"Computer's choice: {cc}")
+print(f"Your choice: {pc}")
